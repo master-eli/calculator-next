ce54ec39d8f53e8c4828f95d9d9e710afb55e01c
Elizear John Acosta Galapia <elizeargalapia.main@gmail.com>
2024-12-09 09:55:09 +0800

    Fix asset loading and deployment configuration

--- next.config.js
+++ next.config.js
@@ -2,9 +2,12 @@
 const nextConfig = {
   output: 'export',
   basePath: '/calculator-next',
+  assetPrefix: '/calculator-next/',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
+  distDir: 'out',
 }
 
 module.exports = nextConfig
