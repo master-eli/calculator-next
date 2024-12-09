01c3be60f51d9414a8e3b2bf2836922fac02583a
master-eli <elizeargalapia.main@gmail.com>
2024-12-09 01:52:17 +0000

    Deploying to gh-pages from @ master-eli/calculator-next@1866248fa6fbed8bc5667b2be77468e647da1108 🚀

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import PriceCalculator from '@/components/PriceCalculator'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gradient-to-br from-purple-50 to-purple-100 dark:from-gray-900 dark:to-purple-950 flex items-center justify-center p-4">
-      <PriceCalculator />
-    </main>
-  )
-}
